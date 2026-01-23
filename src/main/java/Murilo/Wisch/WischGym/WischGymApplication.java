3407e282cf762ae6816d0ff07af9376bd87a58fc
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-23 13:22:46 -0300

    Initial commit - Project structure

---
+++ src/main/java/Murilo/Wisch/WischGym/WischGymApplication.java
@@ -0,0 +1,13 @@
+package Murilo.Wisch.WischGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WischGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WischGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Murilo/Wisch/WischGym/WischGymApplicationTests.java
@@ -0,0 +1,13 @@
+package Murilo.Wisch.WischGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WischGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
