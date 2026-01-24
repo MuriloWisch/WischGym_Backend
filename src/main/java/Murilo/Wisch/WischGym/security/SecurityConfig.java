51a4ed39d084e8c10be3f570c59ebe9065942c0b
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-24 10:38:17 -0300

    Fix: SecuritytConfig

--- src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
+++ src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package Murilo.Wisch.WischGym.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,7 +23,11 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
