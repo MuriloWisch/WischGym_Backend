c6b944dfb6140df916c10b762d4e1fa433cbb901
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-24 17:52:33 -0300

    Add: UserDetailsService

--- src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
+++ src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
@@ -2,16 +2,21 @@ package Murilo.Wisch.WischGym.security;
 
 import Murilo.Wisch.WischGym.domain.User;
 import Murilo.Wisch.WischGym.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
 
@@ -21,12 +26,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email)
-        throws UsernameNotFoundException {
-            User user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("Usuario Não encontrado"));
+            throws UsernameNotFoundException {
 
-            return new org.springframework.security.core.userdetails.User(user.getEmail(),
-                    user.getPassword(), user.getRoles().stream().map
-                    (roles -> new SimpleGrantedAuthority("ROLE_" + roles.name())).collect(Collectors.toSet()));
-        }
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario Não encontrado"));
+
+        System.out.println("EMAIL RECEBIDO: " + email);
+        System.out.println("HASH DO BANCO: " + user.getPassword());
+        System.out.println("TESTE MATCHES: " +
+                passwordEncoder.matches("123456", user.getPassword())
+        );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                        .collect(Collectors.toSet())
+        );
+    }
     }
 
