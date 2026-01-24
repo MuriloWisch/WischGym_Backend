744af089e4ea9de2af63cf632bfaa9a49793fb5a
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-24 10:35:32 -0300

    Feat: securityConfig

--- src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
+++ src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
@@ -1,4 +1,26 @@
 package Murilo.Wisch.WischGym.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic();
+
+
+        return http.build();
+    }
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
+++ src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
@@ -1,21 +1,32 @@
 package Murilo.Wisch.WischGym.security;
 
+import Murilo.Wisch.WischGym.domain.User;
 import Murilo.Wisch.WischGym.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public UserDetailslServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public UserDetails loadUserByUsername{
+    public UserDetails loadUserByUsername(String email)
+        throws UsernameNotFoundException {
+            User user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("Usuario Não encontrado"));
 
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(), user.getRoles().stream().map
+                    (roles -> new SimpleGrantedAuthority("ROLE_" + roles.name())).collect(Collectors.toSet()));
+        }
     }
-}
+
