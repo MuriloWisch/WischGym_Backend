e88bb921dfa0d246bfbf59f6e13e1b08ac3be02a
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-25 19:57:40 -0300

    Feat: AuthenticationController

---
+++ src/main/java/Murilo/Wisch/WischGym/controller/AuthController.java
@@ -0,0 +1,20 @@
+package Murilo.Wisch.WischGym.controller;
+
+import Murilo.Wisch.WischGym.dto.auth.LoginRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+}
