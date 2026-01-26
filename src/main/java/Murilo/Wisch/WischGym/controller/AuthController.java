fd319474d84445b64fe4a3001c8569e8f3cd2c8e
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-25 21:11:47 -0300

    Feat: Authentication Login done

--- src/main/java/Murilo/Wisch/WischGym/controller/AuthController.java
+++ src/main/java/Murilo/Wisch/WischGym/controller/AuthController.java
@@ -2,6 +2,8 @@ package Murilo.Wisch.WischGym.controller;
 
 import Murilo.Wisch.WischGym.dto.auth.LoginRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request){
-
+        Authentication authentication = authenticationManager.authenticate
+                (new UsernamePasswordAuthenticationToken(request.email(),request.password()));
     }
 }
