a2f130281ba7a793fe245fcffbabf941834e41c6
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-26 10:21:15 -0300

    Fix: Bean AuthenticationManager

--- src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
+++ src/main/java/Murilo/Wisch/WischGym/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package Murilo.Wisch.WischGym.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -55,4 +56,11 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
     }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 }
