772fbb555c72174f76086317f3e30cdfbb786750
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-30 13:48:36 -0300

    Test: add UserController

--- src/main/java/Murilo/Wisch/WischGym/controller/UserController.java
+++ src/main/java/Murilo/Wisch/WischGym/controller/UserController.java
@@ -1,4 +1,15 @@
 package Murilo.Wisch.WischGym.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/test")
+    public String User(){
+        return "User ok";
+    }
 }
