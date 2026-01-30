400cbd5e04fca45b8dbb374dec4ffe21f5574b25
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-01-30 13:47:37 -0300

    Test: add ProfessorController

--- src/main/java/Murilo/Wisch/WischGym/controller/ProfessorController.java
+++ src/main/java/Murilo/Wisch/WischGym/controller/ProfessorController.java
@@ -1,4 +1,15 @@
 package Murilo.Wisch.WischGym.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/professor")
 public class ProfessorController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Professor ok";
+    }
 }
