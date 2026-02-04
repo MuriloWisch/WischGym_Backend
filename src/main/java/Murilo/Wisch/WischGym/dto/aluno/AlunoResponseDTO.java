81bb22f1117de21674659c2d6631b1bf2a29f5aa
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-04 16:16:50 -0300

    Feat: AlunoResponseDTO

--- src/main/java/Murilo/Wisch/WischGym/dto/aluno/AlunoResponseDTO.java
+++ src/main/java/Murilo/Wisch/WischGym/dto/aluno/AlunoResponseDTO.java
@@ -1,4 +1,23 @@
 package Murilo.Wisch.WischGym.dto.aluno;
 
+import Murilo.Wisch.WischGym.domain.enums.StatusAlunos;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class AlunoResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private StatusAlunos status;
+    private LocalDateTime dataCadastro;
+
 }
