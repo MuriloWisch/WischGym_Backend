953409fe8650a75baecb4c6a6a42858f7627d9aa
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-05 10:26:48 -0300

    Feat: create AlunoService and create method

---
+++ src/main/java/Murilo/Wisch/WischGym/service/AlunoService.java
@@ -0,0 +1,29 @@
+package Murilo.Wisch.WischGym.service;
+
+import Murilo.Wisch.WischGym.domain.entities.Aluno;
+import Murilo.Wisch.WischGym.dto.aluno.AlunoCreateDTO;
+import Murilo.Wisch.WischGym.dto.aluno.AlunoResponseDTO;
+import Murilo.Wisch.WischGym.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoService {
+
+    private final AlunoRepository alunoRepository;
+
+    public AlunoResponseDTO criar(AlunoCreateDTO dto){
+        Aluno aluno = Aluno.builder()
+                .nome(dto.getNome())
+                .cpf(dto.getCpf())
+                .email(dto.getEmail())
+                .telefone(dto.getTelefone())
+                .dataNascimento(dto.getDataNascimento()).build();
+
+        Aluno salvo = alunoRepository.save(aluno);
+        return toResponseDTO(salvo);
+    }
+
+
+}
