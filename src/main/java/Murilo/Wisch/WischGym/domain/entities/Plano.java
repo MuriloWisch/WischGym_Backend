02660fc8aeeeb6ce83f887552cca8ababa8e71ee
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-06 18:55:52 -0300

    Fix: planoEntitie

--- src/main/java/Murilo/Wisch/WischGym/domain/entities/Plano.java
+++ src/main/java/Murilo/Wisch/WischGym/domain/entities/Plano.java
@@ -1,22 +1,35 @@
 package Murilo.Wisch.WischGym.domain.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "planos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Plano {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(nullable = false)
     private BigDecimal valor;
 
+    @Column(nullable = false)
     private Integer duracaoMeses;
 
+    @Column(nullable = false)
     private Boolean ativo = true;
+
 }
