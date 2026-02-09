a4ce0cb4164f1da272098f7c2feebde164350aaf
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-09 14:51:32 -0300

    Feat: PlanoRepository

---
+++ src/main/java/Murilo/Wisch/WischGym/repository/PlanoRepository.java
@@ -0,0 +1,7 @@
+package Murilo.Wisch.WischGym.repository;
+
+import Murilo.Wisch.WischGym.domain.entities.Plano;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanoRepository extends JpaRepository<Plano, Long> {
+}
