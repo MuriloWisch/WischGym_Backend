3b68af3709c99fe64f2dd753cedfbfb681dcc755
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-09 17:00:54 -0300

    Feat: Listar Plano method

--- src/main/java/Murilo/Wisch/WischGym/controller/PlanoController.java
+++ src/main/java/Murilo/Wisch/WischGym/controller/PlanoController.java
@@ -4,10 +4,9 @@ import Murilo.Wisch.WischGym.domain.entities.Plano;
 import Murilo.Wisch.WischGym.service.PlanoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/planos")
@@ -23,6 +22,12 @@ public class PlanoController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Plano> criar(@RequestBody Plano plano){
         return ResponseEntity.ok(planoService.criar(plano));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','PROFESSOR')")
+    public ResponseEntity<List<Plano>> listar(){
+        return ResponseEntity.ok(planoService.listar());
     }
 
 }
