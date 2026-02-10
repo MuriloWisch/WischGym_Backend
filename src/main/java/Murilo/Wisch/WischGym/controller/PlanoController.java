e2d3cb4b70e7d2d63e08f791fbf534874855306e
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-09 22:26:42 -0300

    Fix: Add Dtos on PlanoController

--- src/main/java/Murilo/Wisch/WischGym/controller/PlanoController.java
+++ src/main/java/Murilo/Wisch/WischGym/controller/PlanoController.java
@@ -3,7 +3,9 @@ package Murilo.Wisch.WischGym.controller;
 import Murilo.Wisch.WischGym.domain.entities.Plano;
 import Murilo.Wisch.WischGym.dto.plano.PlanoCreateDTO;
 import Murilo.Wisch.WischGym.dto.plano.PlanoResponseDTO;
+import Murilo.Wisch.WischGym.dto.plano.PlanoUpdateDTO;
 import Murilo.Wisch.WischGym.service.PlanoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,30 +25,30 @@ public class PlanoController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PlanoResponseDTO> criar(@RequestBody PlanoCreateDTO dto){
+    public ResponseEntity<PlanoResponseDTO> criar(@RequestBody @Valid PlanoCreateDTO dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(planoService.criar(dto));
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN','PROFESSOR')")
-    public ResponseEntity<List<Plano>> listar(){
+    public ResponseEntity<List<PlanoResponseDTO>> listar(){
         return ResponseEntity.ok(planoService.listar());
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN','PROFESSOR')")
-    public ResponseEntity<Plano> buscar(@PathVariable Long id){
+    public ResponseEntity<PlanoResponseDTO> buscar(@PathVariable Long id){
         return ResponseEntity.ok(planoService.buscarPorid(id));
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasHole('ADMIN')")
-    public ResponseEntity<Plano> atualizar(@PathVariable Long id, @RequestBody Plano plano){
-        return ResponseEntity.ok(planoService.atualizar(id, plano));
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PlanoResponseDTO> atualizar(@PathVariable Long id, @RequestBody PlanoUpdateDTO dto){
+        return ResponseEntity.ok(planoService.atualizar(id, dto));
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasHole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         planoService.deletar(id);
         return ResponseEntity.noContent().build();
