bb66d269e41954823e3a68f7873461084ee1dc4d
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-17 15:41:23 -0300

    Feat: MatriculaEntitie

---
+++ src/main/java/Murilo/Wisch/WischGym/domain/Matricula.java
@@ -0,0 +1,40 @@
+package Murilo.Wisch.WischGym.domain;
+
+import Murilo.Wisch.WischGym.domain.entities.Aluno;
+import Murilo.Wisch.WischGym.domain.entities.Plano;
+import Murilo.Wisch.WischGym.domain.enums.StatusMatricula;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "matriculas")
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "plano_id", nullable = false)
+    private Plano plano;
+
+    @Column(nullable = false)
+    private LocalDate dataInicio;
+
+    @Column(nullable = false)
+    private LocalDate dataFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusMatricula status;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+
+}
