8a9b2843d34f4f3884447159c78cb7156958ad62
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-17 15:56:13 -0300

    Feat: MatriculaCreateDTO

---
+++ src/main/java/Murilo/Wisch/WischGym/dto/matricula/MatriculaCreateDTO.java
@@ -0,0 +1,11 @@
+package Murilo.Wisch.WischGym.dto.matricula;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MatriculaCreateDTO {
+    private Long alunoId;
+    private Long planoId;
+}
