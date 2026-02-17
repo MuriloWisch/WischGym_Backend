6b579cb751d7f79566959cf7ff564d5b8571f142
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-02-17 15:54:04 -0300

    Feat: MatriculaRepository

---
+++ src/main/java/Murilo/Wisch/WischGym/repository/MatriculaRepository.java
@@ -0,0 +1,12 @@
+package Murilo.Wisch.WischGym.repository;
+
+import Murilo.Wisch.WischGym.domain.Matricula;
+import Murilo.Wisch.WischGym.domain.enums.StatusMatricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    Optional<Matricula> findByAlunoIdAndStatus(Long alunoId, StatusMatricula status);
+}
