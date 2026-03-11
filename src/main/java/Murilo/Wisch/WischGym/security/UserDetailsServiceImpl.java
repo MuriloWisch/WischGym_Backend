8d9ccd69ac1698ec00734c7a512e7a72f775b390
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-03-11 11:26:00 -0300

    Feat: Swagger authorization config

--- src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
+++ src/main/java/Murilo/Wisch/WischGym/security/UserDetailsServiceImpl.java
@@ -2,21 +2,16 @@ package Murilo.Wisch.WischGym.security;
 
 import Murilo.Wisch.WischGym.domain.User;
 import Murilo.Wisch.WischGym.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
 
