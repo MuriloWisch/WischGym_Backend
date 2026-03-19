61de962281a08fa57b50a46c1be6e217f27cf212
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-03-19 17:53:27 -0300

    Feat: PageableHandlerResolver

--- src/main/java/Murilo/Wisch/WischGym/config/CorsConfig.java
+++ src/main/java/Murilo/Wisch/WischGym/config/CorsConfig.java
@@ -2,6 +2,9 @@ package Murilo.Wisch.WischGym.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +22,14 @@ public class CorsConfig {
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
+        };
+
+    }
+    @Bean
+    public PageableHandlerMethodArgumentResolverCustomizer pageableCustomizer() {
+        return resolver -> {
+            resolver.setFallbackPageable(PageRequest.of(0, 10, Sort.by("nome")));
+            resolver.setMaxPageSize(100);
         };
     }
 }
