c8f048eadb4f9a2cb6273244649824eb720ed814
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-03-24 16:15:45 -0300

    Feat: enums treinos

---
+++ src/main/java/Murilo/Wisch/WischGym/domain/enums/DivisãoTreino.java
@@ -0,0 +1,17 @@
+package Murilo.Wisch.WischGym.domain.enums;
+
+public enum DivisãoTreino {
+    FULL_BODY,
+    UPPER_LOWER,
+    AB,
+    ABC,
+    ABCD,
+    ABCDE,
+    PPL,
+    PPL_FULL_BODY,
+    UPPER_LOWER_2X,
+    PPL_UPPER_LOWER,
+    ANTAGONISTA,
+    ARNOLD_SPLIT,
+    BRO_SPLIT
+}
---
+++ src/main/java/Murilo/Wisch/WischGym/domain/enums/GrupoMuscular.java
@@ -0,0 +1,17 @@
+package Murilo.Wisch.WischGym.domain.enums;
+
+public enum GrupoMuscular {
+    PEITO,
+    COSTAS,
+    OMBROS,
+    BICEPS,
+    TRICEPS,
+    ANTEBRACO,
+    QUADRICEPS,
+    POSTERIOR,
+    GLUTEOS,
+    PANTURRILHA,
+    ABDOMEN,
+    TRAPEZIO,
+    FULL_BODY
+}
