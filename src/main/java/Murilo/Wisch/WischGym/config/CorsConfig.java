6b6088676d78c6c54acf1159683b439535857e2e
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-04-02 13:33:36 -0300

    Feat: cors config

--- src/main/java/Murilo/Wisch/WischGym/config/CorsConfig.java
+++ src/main/java/Murilo/Wisch/WischGym/config/CorsConfig.java
@@ -5,26 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+    public CorsConfigurationSource corsConfigurationSource() {
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+        CorsConfiguration config = new CorsConfiguration();
 
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://wischgym-gueth6sq7-murilowischs-projects.vercel.app"
+        ));
 
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
+
     @Bean
     public PageableHandlerMethodArgumentResolverCustomizer pageableCustomizer() {
         return resolver -> {
